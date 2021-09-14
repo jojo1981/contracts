b3c5e1978348699b16d021a1fc5123480c2b66e7
Joost Nijhuis <jnijhuis81@gmail.com>
2021-09-14 12:02:30 +0200

    Add clearable interface.

---
+++ src/ClearableInterface.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of the jojo1981/contracts package
+ *
+ * Copyright (c) 2021 Joost Nijhuis <jnijhuis81@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed in the root of the source code
+ */
+namespace Jojo1981\Contracts;
+
+/**
+ * @package Jojo1981\Contracts
+ */
+interface ClearableInterface
+{
+    /**
+     * Clears all data this data container contains
+     *
+     * @return void
+     */
+    public function clear(): void;
+}
