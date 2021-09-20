bee05182d998ca9d7907a68e696199b5fd028ca6
Joost Nijhuis <jnijhuis81@gmail.com>
2021-09-20 09:26:06 +0200

    Add AbstractComparable helper class for easily creating a comparable class. Only the `compareTo` method has to be implemented.

---
+++ src/AbstractComparable.php
@@ -0,0 +1,72 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of the jojo1981/contracts package
+ *
+ * Copyright (c) 2021 Joost Nijhuis <jnijhuis81@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed in the root of the source code
+ */
+namespace Jojo1981\Contracts;
+
+/**
+ * @package Jojo1981\Contracts
+ * @template T of ComparableInterface
+ * @template-implements ComparableInterface<T>
+ */
+abstract class AbstractComparable implements ComparableInterface
+{
+    /**
+     * @param T $object
+     * @return bool
+     */
+    public function isLowerThan($object): bool
+    {
+        return $this->compareTo($object) < 0;
+    }
+
+    /**
+     * @param T $object
+     * @return bool
+     */
+    public function isLowerThanOrEqualTo($object): bool
+    {
+        return $this->compareTo($object) <= 0;
+    }
+
+    /**
+     * @param T $object
+     * @return bool
+     */
+    public function isEqual($object): bool
+    {
+        return 0 === $this->compareTo($object);
+    }
+
+    /**
+     * @param T $object
+     * @return bool
+     */
+    public function isHigherThanOrEqualTo($object): bool
+    {
+        return $this->compareTo($object) >= 0;
+    }
+
+    /**
+     * @param T $object
+     * @return bool
+     */
+    public function isHigherThan($object): bool
+    {
+        return $this->compareTo($object) > 0;
+    }
+
+    /**
+     * @param T $object
+     * @return bool
+     */
+    public function isNotEqual($object): bool
+    {
+        return !$this->isEqual($object);
+    }
+}
